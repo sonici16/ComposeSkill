cd0c350058ad9d8016f0c8da0ed876a97c5115f7
sm <sonici@smtown.com>
2025-09-19 01:14:16 +0900

    delete

--- ComposeSkill/app/src/main/java/com/sonici16/composeskill/MyReceiptApp.kt
+++
@@ -1,7 +0,0 @@
-package com.sonici16.composeskill
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyReceiptApp : Application()
--- ComposeSkill/app/src/main/java/com/sonici16/composeskill/ui/theme/model/RecipeUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.sonici16.composeskill.ui.theme.model
-
-sealed class RecipeUiState {
-    object Loading : RecipeUiState()
-    data class Success(val recipes: List<Row>) : RecipeUiState()
-    data class Error(val message: String) : RecipeUiState()
-}
